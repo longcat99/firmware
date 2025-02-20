11ce759fb23b67476ac286c9e756fa95a315e001
Huang jinlong <33887488+longcat99@users.noreply.github.com>
2025-02-21 05:28:42 +0800

    Update baresip-openipc.mk

--- general/package/baresip-openipc/baresip-openipc.mk
+++ general/package/baresip-openipc/baresip-openipc.mk
@@ -20,14 +20,10 @@ define BARESIP_OPENIPC_INSTALL_CONF
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(BARESIP_OPENIPC_PKGDIR)/files/S97baresip
 
-#	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/baresip
-#	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/baresip $(BARESIP_OPENIPC_PKGDIR)/files/accounts
-#	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/baresip $(BARESIP_OPENIPC_PKGDIR)/files/config
-#	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/baresip $(BARESIP_OPENIPC_PKGDIR)/files/contacts
-
-	cp $(@D)/docs/examples/accounts $(TARGET_DIR)/etc/baresip/accounts.orig
-	cp $(@D)/docs/examples/config $(TARGET_DIR)/etc/baresip/config.orig
-	cp $(@D)/docs/examples/contacts $(TARGET_DIR)/etc/baresip/contacts.orig
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/baresip
+	$(INSTALL) -m 644 $(BARESIP_OPENIPC_PKGDIR)/files/accounts $(TARGET_DIR)/etc/baresip/accounts.orig
+	$(INSTALL) -m 644 $(BARESIP_OPENIPC_PKGDIR)/files/config $(TARGET_DIR)/etc/baresip/config.orig
+	$(INSTALL) -m 644 $(BARESIP_OPENIPC_PKGDIR)/files/contacts $(TARGET_DIR)/etc/baresip/contacts.orig
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BARESIP_OPENIPC_PKGDIR)/files/dtmf_0.sh
@@ -35,12 +31,10 @@ define BARESIP_OPENIPC_INSTALL_CONF
 	ln -sf dtmf_0.sh $(TARGET_DIR)/usr/bin/dtmf_2.sh
 	ln -sf dtmf_0.sh $(TARGET_DIR)/usr/bin/dtmf_3.sh
 
-#	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sounds
-#	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/lib/sounds $(BARESIP_OPENIPC_PKGDIR)/files/ok_8k.pcm
-
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/baresip
-	cp $(@D)/share/*.wav $(TARGET_DIR)/usr/share/baresip
+	$(INSTALL) -m 644 $(@D)/share/*.wav $(TARGET_DIR)/usr/share/baresip
 endef
+
 
 BARESIP_OPENIPC_POST_INSTALL_TARGET_HOOKS += BARESIP_OPENIPC_INSTALL_CONF
 
